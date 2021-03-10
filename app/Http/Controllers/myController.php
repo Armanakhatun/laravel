84d624728b1446545e4b3648c06d7d2eceb5006f
Armanakhatun <armanakhatun1@gmail.com>
2021-03-10 08:31:46 +0545

    seventh commit

--- app/Http/Controllers/myController.php
+++ app/Http/Controllers/myController.php
@@ -60,6 +60,12 @@ class myController extends Controller
         $showdata=Product::orderBy('id','desc')->get();
         return view('showproduct',['showdata'=>$showdata]);
     }
+     public function homepage()
+    {
+        $show=Product::orderBy('id','desc')->get();
+        return view('homepage',['show'=>$show]);
+    }
+
 
     /**
      * Show the form for editing the specified resource.
